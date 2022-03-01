8d3a498bdb6e02ddd3d5a7b552eb32538e9d5823
Mira Winkel <mira@reflectionwebdesign.com>
2022-03-01 20:21:55 +0000

    refactored file structure, removed example, added payments and logic

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,40 +1,21 @@
-{{ config(
-    materialized="table"
-)}}
-
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    
-    from raw.jaffle_shop.customers
-    
+    select * from {{ref('stg_customers')}}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    
-    from raw.jaffle_shop.orders
+    select * from {{ref('fct_orders')}}
 ),
 
 customer_orders as (
 
     select
         customer_id,
-    
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     
     from orders
-    
     group by 1
 ),
 
@@ -46,7 +27,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
     
     from customers
     
@@ -54,3 +36,5 @@ final as (
 )
 
 select * from final
+-- select sum(lifetime_value)
+-- from final
